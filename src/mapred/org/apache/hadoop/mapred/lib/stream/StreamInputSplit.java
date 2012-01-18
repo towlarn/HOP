90ab1df85bd9a31bc39b5dc287f51426f8813d24
Thanat Owlarn <towlarn@seas.upenn.edu>
2012-01-18 16:10:00 -0500

    asd

--- src/mapred/org/apache/hadoop/mapred/lib/stream/StreamInputSplit.java
+++ src/mapred/org/apache/hadoop/mapred/lib/stream/StreamInputSplit.java
@@ -3,12 +3,16 @@ package org.apache.hadoop.mapred.lib.stream;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.net.InetAddress;
 
+import org.apache.hadoop.io.UTF8;
 import org.apache.hadoop.mapred.InputSplit;
 
 public class StreamInputSplit implements InputSplit {
 	
-	//testing comment
+	private InetAddress addr;
+	private int port;
+
 	@Override
 	public long getLength() throws IOException {
 		// Return 0 for now, since it is an infinite stream.
@@ -17,20 +21,26 @@ public class StreamInputSplit implements InputSplit {
 
 	@Override
 	public String[] getLocations() throws IOException {
-		// TODO Auto-generated method stub
+		// Each element of array is a <host,port> pair of incoming stream
 		return null;
 	}
 
 	@Override
 	public void readFields(DataInput in) throws IOException {
-		// TODO Auto-generated method stub
-
+		addr = InetAddress.getByName(UTF8.readString(in));
+		port = in.readInt();
 	}
 
 	@Override
 	public void write(DataOutput out) throws IOException {
-		// TODO Auto-generated method stub
-
+		UTF8.writeString(out, addr.toString());
+		out.writeInt(port);		
+	}
+	
+	public static StreamInputSplit read(DataInput in) throws IOException {
+		StreamInputSplit w = new StreamInputSplit();
+		w.readFields(in);
+		return w;
 	}
 
 }
